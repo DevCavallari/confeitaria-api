364158cf6fc1ad09c074c5a5c17f3936614a98c0
viniciuscavallari9 <viniciuscavallari9@gmail.com>
2025-11-19 00:35:05 +0000

    teste 1.2

--- cardapio.py
+++ cardapio.py
@@ -6,8 +6,18 @@ def buscar_cardapio():
             "nome": "Chocomoca",
             "descricao": " Bolo de chocolate com ninho ",
             "preco": 200.99,
-            "foto": ""
-
+        
         }
     ]
     return itens
+
+    def buscar_cardapio_id(item_id):
+        return{
+        
+            "id": 1,
+            "nome": "Chocomoca",
+            "descricao": " Bolo de chocolate com ninho ",
+            "preco": 200.99,
+            "foto": ""
+        
+        }
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
 from flask import Flask
-from cardapio import buscar_cardapio
+from cardapio import buscar_cardapio, buscar_cardapio_id
+
 
 
 app = Flask(__name__)
@@ -16,6 +17,9 @@ def hello_world():
 def mostrar_cardapio():
     return buscar_cardapio()
 
+@app.route("/cardapio/<int:item_id>")
+def mostrar_cardapio_id(item_id):
+    return buscar_cardapio_id(item_id)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
