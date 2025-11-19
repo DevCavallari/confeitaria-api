48e7cd8757c48b42ca8202cc27f8fbc02d8eea87
viniciuscavallari9 <viniciuscavallari9@gmail.com>
2025-11-19 01:15:43 +0000

    1.4

--- cardapio.py
+++ cardapio.py
@@ -1,21 +1,36 @@
+from conexao import get_conexao
+from psycopg2.extras import RealDictCursor
+from flask import jsonify
+
+def buscar_cardapio():
+    conexao = get_conexao()
+    cursor = conexao.cursor(cursor_factory=RealDictCursor)
+    cursor.execute("SELECT * FROM cardapio")
+    itens = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(cardapio)
+
 
 def buscar_cardapio():
     itens = [
         {
             "id": 1,
-            "nome": "Chocomoca",
+            "nome": "Chocomoça",
             "descricao": " Bolo de chocolate com ninho ",
             "preco": 200.99,
-        
+            "foto": ""
+
         }
     ]
     return itens
 
-    def buscar_cardapio_id(item_id):
-        return{
-        
+def buscar_cardapio_id(item_id):
+    return{
             "id": 1,
-            "nome": "Chocomoca",
+            "nome": "Chocomoça",
             "descricao": " Bolo de chocolate com ninho ",
-            "preco": 200.99,
-        }
+            "preco": 150.99,
+            "foto": ""
+    }
+
--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import os
 from flask import Flask
 from cardapio import buscar_cardapio, buscar_cardapio_id
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+  return {
+    "message": "Api rodando!"
+  }
 
 @app.route("/cardapio")
-def mostrar_cardapio():
-    return buscar_cardapio()
+def cardapio():
+  return buscar_cardapio()
 
 @app.route("/cardapio/<int:item_id>")
-def mostrar_cardapio_id(item_id):
-    return buscar_cardapio_id(item_id)
+def cardapio_id(item_id):
+  return buscar_cardapio_id(item_id)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
